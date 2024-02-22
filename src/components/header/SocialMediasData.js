9512109cace6b5928506e78f53e5471eff9e678e
SoriaHaidari <133559476+SoriaHaidari@users.noreply.github.com>
2024-02-22 15:20:13 +0430

    Update SocialMediasData.js
    Social media links changed.

--- src/components/header/SocialMediasData.js
+++ src/components/header/SocialMediasData.js
@@ -6,17 +6,17 @@ import { AiFillGithub } from "react-icons/ai";
 const data = [
   {
     id: 1,
-    link: "https://instagram.com/soria_haidari_",
+    link: "https://www.instagram.com/suriyahaidari?igsh=ajk2ZGoxaWhudDQw",
     icon: <AiOutlineInstagram />,
   },
   {
     id: 2,
-    link: "https://twitter.com/Soria_Haidari",
+    link: "https://twitter.com/Suriya_Haidari",
     icon: <AiOutlineTwitter />,
   },
   {
     id: 3,
-    link: "https://linkedin.com/in/SoriaHaidari",
+    link: "https://www.linkedin.com/in/suriya-haidari-903a6727b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
     icon: <CiLinkedin />,
   },
   { id: 4, link: "https://github.com/soriahaidari", icon: <AiFillGithub /> },
